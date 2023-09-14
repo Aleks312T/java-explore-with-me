a841d37ec77874ec1b6bc6eaa95f23cc20400309
Aleks <aleks312t@gmail.com>
2023-09-14 07:26:46 +0300

    style: мелкое изменение, для тестирования

--- main-service/src/main/java/ru/practicum/explore_with_me/ExploreWithMeServer.java
+++ main-service/src/main/java/ru/practicum/explore_with_me/ExploreWithMeServer.java
@@ -3,6 +3,7 @@ package ru.practicum.explore_with_me;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//Комментарий, чтобы репозиторий протестировался
 @SpringBootApplication(scanBasePackages = {"ru.practicum.stats_client", "ru.practicum.explore_with_me"})
 public class ExploreWithMeServer {
     public static void main(String[] args) {
